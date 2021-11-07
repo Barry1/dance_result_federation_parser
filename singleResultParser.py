0c54b7789454dde24e11f711ff114db7b56cdc98
Ebeling, Dr. Bastian <230051+Barry1@users.noreply.github.com>
2021-11-07 14:29:52 +0100

    single result fine

--- singleResultParser.py
+++ singleResultParser.py
@@ -1,3 +1,4 @@
+"""Module analyzing single competitions not events."""
 from resultParser import interpret_tt_result, print_tsh_web
 
 if __name__ == "__main__":
@@ -6,7 +7,8 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         for theurl in sys.argv[1:]:
             print(f"Auswertung von {theurl}")
-            eventurl_to_web(theurl)
+            print_tsh_web([theurl], [interpret_tt_result(theurl)])
     else:
-        theurl = "https://www.tbw.de/turnierergebnisse/2021/2021_11_06_Boeblingen_dm_hgrsstd/index.htm"
-    print_tsh_web([theurl], [interpret_tt_result(theurl)])
+        THEURL = "https://www.tbw.de/turnierergebnisse/2021/2021_11_06_Boeblingen_dm_hgrsstd/index.htm"
+        print(f"Auswertung von {THEURL}")
+        print_tsh_web([THEURL], [interpret_tt_result(THEURL)])
