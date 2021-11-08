4057501f8fb01006572e94cb2249ad4f8ed60805
Ebeling, Dr. Bastian <230051+Barry1@users.noreply.github.com>
2021-11-08 21:20:39 +0100

    now async (default) implemented

--- resultParser.py
+++ resultParser.py
@@ -4,23 +4,54 @@
 urllib.request für https nötig
 https://www.w3schools.com/xml/xpath_syntax.asp
 """
+import asyncio
 from typing import Callable, cast
 from urllib.error import HTTPError
 from urllib.request import urlopen
-
 from joblib import Parallel, delayed
-
-# from lxml.etree import _ElementTree
 from lxml.html import parse
 from pandas import DataFrame
 from pandas import set_option as pandas_set_option
 from valuefragments import ic
-
 from dtvprocessing import get_dtv_df
 from topturnierprocessing import checkttontree, interpret_tt_result, srparserurl
 from tpsprocessing import checktpsontree, interpret_tps_result, ogparserurl
 
+RUN_ASYNC = True
 pandas_set_option("mode.chained_assignment", "raise")  # warn,raise,None
+
+
+async def async_eventurl_to_web(eventurl: str) -> None:
+    """Async convert URL from Event to HTML for TSH CMS."""
+    try:
+        with urlopen(eventurl) as openedurl:
+            tree = await asyncio.to_thread(parse, openedurl)
+    except HTTPError as http_error:
+        print(http_error)
+    else:
+        theparsefun: Callable[[str], dict[str, str]]
+        the_interpret_fun: Callable[[str], DataFrame]
+        if checktpsontree(tree):
+            ic(f"{eventurl} ist eine TPS-Veranstaltung")
+            theparsefun = ogparserurl
+            the_interpret_fun = interpret_tps_result
+        elif checkttontree(tree):
+            ic(f"{eventurl} ist eine TT-Veranstaltung")
+            theparsefun = srparserurl
+            the_interpret_fun = interpret_tt_result
+        else:
+            print(f"Die URL {eventurl} kann weder TPS noch TT zugeordnet werden.")
+            return
+        allreslinks = theparsefun(eventurl).values()
+        tsh_results: list[DataFrame] = list(
+            await asyncio.gather(
+                *(
+                    asyncio.to_thread(the_interpret_fun, a)
+                    for a in theparsefun(eventurl).values()
+                )
+            )
+        )
+        print_tsh_web(list(allreslinks), tsh_results)
 
 
 def eventurl_to_web(eventurl: str) -> None:
@@ -110,7 +141,10 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         for theurl in sys.argv[1:]:
             print(f"Auswertung von {theurl}")
-            eventurl_to_web(theurl)
+            if RUN_ASYNC:
+                asyncio.run(async_eventurl_to_web(theurl))
+            else:
+                eventurl_to_web(theurl)
     else:
         print("Selbsttest des Moduls resultParser")
         print(get_dtv_df().loc[403:406])
