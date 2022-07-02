a3e38f1e4f5cab0231b915ad82da4a58a8cf8ed1
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2022-07-02 23:48:47 +0200

    some fixes and logging

--- singleResultParser.py
+++ singleResultParser.py
@@ -1,17 +1,30 @@
 """Module analyzing single competitions not events."""
 import logging
 
-logging.getLogger("TSH.resultParser")
 from resultParser import interpret_tt_result, print_tsh_web
 
+thelogger = logging.getLogger("TSH.singleResultParser")
+logformatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)  # https://docs.python.org/3/library/logging.html#logrecord-attributes
+logfilehandler = logging.FileHandler("singleResultParser.log")
+logfilehandler.setFormatter(logformatter)
+thelogger.addHandler(logfilehandler)
+if __debug__:
+    thelogger.setLevel(logging.DEBUG)
+else:
+    thelogger.setLevel(logging.INFO)
 if __name__ == "__main__":
     import sys
 
     if len(sys.argv) > 1:
         for theurl in sys.argv[1:]:
-            logging.info(f"Auswertung von {theurl}")
+            thelogger.info("Auswertung von %s", theurl)
             print_tsh_web([theurl], [interpret_tt_result(theurl)])
     else:
-        THEURL = "https://www.tbw.de/turnierergebnisse/2021/2021_11_06_Boeblingen_dm_hgrsstd/index.htm"
-        logging.info(f"Auswertung von {THEURL}")
+        THEURL = (
+            "https://www.tbw.de/turnierergebnisse/2021"
+            + "/2021_11_06_Boeblingen/index.htm"
+        )
+        thelogger.info("Auswertung von %s", THEURL)
         print_tsh_web([THEURL], [interpret_tt_result(THEURL)])
