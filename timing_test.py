29529bb0433596f7a2d9b095867d41e924d8cf7a
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2022-10-02 13:52:40 +0200

    first timing_test sync vs. async

---
+++ timing_test.py
@@ -0,0 +1,23 @@
+#! poetry run python -OO timing_test.py
+import asyncio
+
+from valuefragments import TimingCM
+
+from dance_result_federation_parser import (
+    async_eventurl_to_web,
+    eventurl_to_web,
+)
+
+if __name__ == "__main__":
+    theurl: str = (
+        "http://blauesband-berlin.de/Ergebnisse/2019/blauesband2019/index.htm"
+    )
+    RUN_ASYNC = False
+    print("Running Sync")
+    with TimingCM():
+        eventurl_to_web(theurl)
+    RUN_ASYNC = True
+    print("Running ASync")
+    with TimingCM():
+        asyncio.run(async_eventurl_to_web(theurl))
+    print("Hallo")
