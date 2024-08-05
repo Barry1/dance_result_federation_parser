7daf1c4340bb729fb97819791a71a5e323578e4b
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2024-08-05 13:49:58 +0200

    output in presentation layer

--- single_result_parser.py
+++ single_result_parser.py
@@ -6,11 +6,8 @@ from typing import Callable
 from pandas import DataFrame
 
 from configprocessing import MyConfigT, readconfig, setuplogger
-from dance_result_federation_parser import (
-    interpret_tt_result,
-    print_markdown,
-    print_tsh_web,
-)
+from dance_result_federation_parser import interpret_tt_result
+from presentationlayer import print_markdown, print_tsh_web
 
 thelogger: logging.Logger = setuplogger()
 _CFG_DICT: MyConfigT = readconfig()
