b3ed5f34f24c0e49effc4d0ae03302536cc65922
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2024-08-18 16:28:08 +0200

    formatting and updating

--- goc_parser.py
+++ goc_parser.py
@@ -16,16 +16,13 @@ if __name__ == "__main__":
         a for a in foundreslinks if a.endswith("index.htm")
     ]
     compnames: list[str] = [
-        human_comp_info(thelink) for thelink in allreslinks
+        human_comp_info(turnier_info=thelink) for thelink in allreslinks
     ]
-    tsh_results: list[DataFrame] = cast(
-        list[DataFrame],
-        Parallel(
-            n_jobs=1 if __debug__ else -1,
-            verbose=10 if __debug__ else 0,
-            backend="multiprocessing",
-        )(delayed(the_interpret_fun)(a) for a in allreslinks),
-    )
+    tsh_results: list[DataFrame] = Parallel(
+        n_jobs=1 if __debug__ else -1,
+        verbose=10 if __debug__ else 0,
+        backend="multiprocessing",
+    )(delayed(function=the_interpret_fun)(a) for a in allreslinks)
     presentation_function(
         "https://www.goc-stuttgart.de/event-guide/ergebnisarchiv",
         list(allreslinks),
