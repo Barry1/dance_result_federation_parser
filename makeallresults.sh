c7c73f11a2ee63ec16b56ed2512017913c718ee7
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2024-09-21 18:08:37 +0200

    seems to work

---
+++ makeallresults.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#https://unix.stackexchange.com/a/230050
+#for a in $(make -qp | awk -F':' '/^[a-zA-Z0-9][^$#\/\t=]*:([^=]|$)/ {split($1,A,/ /);for(i in A)print A[i]}' | grep txt)
+#    do make "$a"
+#done
+make --ignore-errors --always-make $(make -qp | awk -F':' '/^[a-zA-Z0-9][^$#\/\t=]*:([^=]|$)/ {split($1,A,/ /);for(i in A)print A[i]}' | grep txt)
