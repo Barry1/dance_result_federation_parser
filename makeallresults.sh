bf09fbba11aeb3ff6fe6982caf828edb84860043
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2025-05-19 23:19:55 +0200

    bit better

--- makeallresults.sh
+++ makeallresults.sh
@@ -3,4 +3,13 @@
 #for a in $(make -qp | awk -F':' '/^[a-zA-Z0-9][^$#\/\t=]*:([^=]|$)/ {split($1,A,/ /);for(i in A)print A[i]}' | grep txt)
 #    do make "$a"
 #done
-make --jobs --ignore-errors --always-make $(make -qp | awk -F':' '/^[a-zA-Z0-9][^$#\/\t=]*:([^=]|$)/ {split($1,A,/ /);for(i in A)print A[i]}' | grep txt)
+
+targets=$(make -qp |
+	awk -F':' '/^[a-zA-Z0-9][^$#\/\t=]*:([^=]|$)/ {split($1,A,/ /);for(i in A)print A[i]}' |
+	grep txt |
+	grep -v GOC_ |
+	sort)
+goctargets="GOC_2020.txt GOC_2021.txt GOC_2022.txt GOC_2023.txt GOC_2024.txt GOC_2025.txt"
+alltargets="$targets $goctargets"
+echo "Targets: $alltargets"
+make --jobs 1 --ignore-errors --always-make $alltargets
