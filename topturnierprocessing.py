c1c472aa7e8fab853ba4c5fe5243ddf69db5b4eb
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2026-03-18 08:07:38 +0100

    fastparquet 2026.3.0 not available

--- topturnierprocessing.py
+++ topturnierprocessing.py
@@ -129,7 +129,6 @@ def tt_from_erg(theresultresponse: Response) -> DataFrame:
             # Solisten haben keine Vereine
             erg_df["Verein"] = "∅"
         erg_df = erg_df.iloc[:, [0, 1, 2]]
-    # erg_df = erg_df.set_axis(        ["Platz", "Paar", "Verein"], axis="columns", copy=False    )
     erg_df = erg_df.set_axis(
         labels=["Platz", "Paar", "Verein"], axis="columns"
     )
