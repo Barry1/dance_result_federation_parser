8b4bfc1d6d03b4f2a45b5cc7554c61e55a22a3db
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2026-05-11 08:47:06 +0200

    new is faster

--- makeallresults.sh
+++ makeallresults.sh
@@ -4,13 +4,17 @@
 #    do make "$a"
 #done
 
-targets=$(make -qp |
-	awk -F':' '/^[a-zA-Z0-9][^$#\/\t=]*:([^=]|$)/ {split($1,A,/ /);for(i in A)print A[i]}' |
-	grep txt |
-	grep -v GOC_ |
-	sort)
-goctargets="\nGOC_2020.txt\nGOC_2021.txt\nGOC_2022.txt\nGOC_2023.txt\nGOC_2024.txt\nGOC_2025.txt"
-alltargets="${targets} ${goctargets}"
+# OLD STYLE
+#targets=$(make -qp |
+#	awk -F':' '/^[a-zA-Z0-9][^$#\/\t=]*:([^=]|$)/ {split($1,A,/ /);for(i in A)print A[i]}' |
+#	grep txt |
+#	grep -v GOC_ |
+#	sort)
+#goctargets="\nGOC_2020.txt\nGOC_2021.txt\nGOC_2022.txt\nGOC_2023.txt\nGOC_2024.txt\nGOC_2025.txt"
+#alltargets="${targets} ${goctargets}"
+# OLD STYLE
+
+alltargets=$(make list)
 mdtargets=$(echo "${alltargets}" | shuf | sed -e 's/.txt/.md/g')
 # shuffled to be not so reproducible
 echo "Targets: ${mdtargets}"
