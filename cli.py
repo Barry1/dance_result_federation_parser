8fc0bad4a2893b85e5d3e70c3bc1ea2a5e8f29e5
Dr. Bastian Ebeling <230051+Barry1@users.noreply.github.com>
2026-05-12 14:59:35 +0200

    formatted and small steps

--- cli.py
+++ cli.py
@@ -1,5 +1,7 @@
 from argparse import ArgumentParser
 from configprocessing import MyConfigT, readconfig
+from dance_result_federation_parser import async_eventurl_to_web, eventurl_to_web
+import asyncio
 
 drfparser = ArgumentParser(
     prog="drfp", description="Dance Result Federation Parser", epilog="epi"
@@ -8,7 +10,10 @@ drfparser.add_argument("URL", type=str)
 drfparser.add_argument("-s", "--single", default=False, action="store_true")
 drfparser.add_argument("-v", "--verbose", default=False, action="store_true")
 drfparser.add_argument(
-    "-f", "--format", type=str.lower, choices=["joomla", "typo", "wordpress", "markdown"]
+    "-f",
+    "--format",
+    type=str.lower,
+    choices=["joomla", "typo", "wordpress", "markdown"],
 )
 drfparser.add_argument("-o", "--output", type=str)
 
@@ -18,10 +23,16 @@ def main() -> None:
     args = drfparser.parse_args()
     print(args)
     if args.format:
-        _ConfigDict["RESULTFORMAT"] = args.format
+        _ConfigDict["RESULTFORMAT"] = args.format.upper()
     print(_ConfigDict)
     if args.single:
         print("single")
+    else:
+        print("multi")
+        if _ConfigDict["RUN_ASYNC"]:
+            asyncio.run(async_eventurl_to_web(args.URL), debug=__debug__)
+        else:
+            eventurl_to_web(args.URL)
 
 
 if __name__ == "__main__":
